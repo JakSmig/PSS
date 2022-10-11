78b361d7cfbfb6de6d42ad668c15f29d176eba95
JakSmig <248908@student.pwr.edu.pl>
2022-10-11 08:47:09 +0200

    Base spring project

---
+++ src/main/java/com/pwr/StoliceSwiata/StoliceSwiataApplication.java
@@ -0,0 +1,13 @@
+package com.pwr.StoliceSwiata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoliceSwiataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoliceSwiataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwr/StoliceSwiata/StoliceSwiataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwr.StoliceSwiata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoliceSwiataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
