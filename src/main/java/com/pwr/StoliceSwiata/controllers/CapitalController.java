226de80ec0638ee0d98c6b3eef1b32c92af971f3
JakSmig <248908@student.pwr.edu.pl>
2022-10-25 10:24:13 +0200

    Fixed CrossOrigin

---
+++ src/main/java/com/pwr/StoliceSwiata/controllers/CapitalController.java
@@ -0,0 +1,13 @@
+package com.pwr.StoliceSwiata.controllers;
+
+import com.pwr.StoliceSwiata.dbSchema.Capital;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@Controller
+@RequestMapping(path="/capital")
+public class CapitalController {
+
+}
